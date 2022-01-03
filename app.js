31bb577532e5b5936d85e42173c8ffac266d34bc
NealLyonsWake <waken.games@gmail.com>
2022-01-03 21:44:00 +0000

    Update

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(
   cors({
     origin: 'http://localhost:3000'
